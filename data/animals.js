26218bbebae4a7123d8cbf5d6e272c644d39649a
Joseph-K-B <303josephb@gmail.com>
2021-08-13 11:42:31 -0700

    GET not working, fixed server error from last app

--- data/animals.js
+++ data/animals.js
@@ -1,14 +1,86 @@
 module.exports = [
   {
-    name: 'bessie',
-    cool_factor: 3,
+    id:1,
+    key: 'c-major',
+    chord: 'C-major',
+    major: true,
+    class: 'primary'
   },
   {
-    name: 'jumpy',
-    cool_factor: 4,
+    id:2,
+    key: 'c-major',
+    chord: 'D-minor',
+    major: false,
+    class: 'primary'
   },
   {
-    name: 'spot',
-    cool_factor: 10,
-  }
+    id:3,
+    key: 'c-major',
+    chord: 'E-minor',
+    major: false,
+    class: 'primary'
+  },
+  {
+    id:4,
+    key: 'c-major',
+    chord: 'F-major',
+    major: true,
+    class: 'primary'
+  },
+  {
+    id:5,
+    key: 'c-major',
+    chord: 'G-major',
+    major: true,
+    class: 'primary'
+  },
+  {
+    id:6,
+    key: 'c-major',
+    chord: 'A-minor',
+    major: false,
+    class: 'primary'
+  },
+  {
+    id:7,
+    key: 'c-major',
+    chord: 'C-major7',
+    major: true,
+    class: 'secondary'
+  },
+  {
+    id:8,
+    key: 'c-major',
+    chord: 'D-minor7',
+    major: false,
+    class: 'secondary'
+  },
+  {
+    id:9,
+    key: 'c-major',
+    chord: 'E-minor7',
+    major: false,
+    class: 'secondary'
+  },
+  {
+    id:10,
+    key: 'c-major',
+    chord: 'F-major7',
+    major: true,
+    class: 'secondary'
+  },
+  {
+    id:11,
+    key: 'c-major',
+    chord: 'G-dominant7',
+    major: false,
+    class: 'secondary'
+  },
+  {
+    id:12,
+    key: 'c-major',
+    chord: 'A-minor7',
+    major: false,
+    class: 'secondary'
+  },
 ];

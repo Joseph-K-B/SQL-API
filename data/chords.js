8bf773fa5f8e838763985e5e4062899ff92ce8e5
Joseph-K-B <303josephb@gmail.com>
2021-08-20 16:47:16 -0700

    changed key to musical_key

--- data/chords.js
+++ data/chords.js
@@ -1,84 +1,84 @@
 module.exports = [
   {
     id:1,
-    key: 'c-major',
+    musical_key: 'c-major',
     chord: 'C-major',
     major: true,
     class_id: 1,
   },
   {
     id:2,
-    key: 'c-major',
+    musical_key: 'c-major',
     chord: 'D-minor',
     major: false,
     class_id: 1
   },
   {
     id:3,
-    key: 'c-major',
+    musical_key: 'c-major',
     chord: 'E-minor',
     major: false,
     class_id: 1
   },
   {
     id:4,
-    key: 'c-major',
+    musical_key: 'c-major',
     chord: 'F-major',
     major: true,
     class_id: 1
   },
   {
     id:5,
-    key: 'c-major',
+    musical_key: 'c-major',
     chord: 'G-major',
     major: true,
     class_id: 1,
   },
   {
     id:6,
-    key: 'c-major',
+    musical_key: 'c-major',
     chord: 'A-minor',
     major: false,
     class_id: 1
   },
   {
     id:7,
-    key: 'c-major',
+    musical_key: 'c-major',
     chord: 'C-major7',
     major: true,
     class_id: 2
   },
   {
     id:8,
-    key: 'c-major',
+    musical_key: 'c-major',
     chord: 'D-minor7',
     major: false,
     class_id: 2
   },
   {
     id:9,
-    key: 'c-major',
+    musical_key: 'c-major',
     chord: 'E-minor7',
     major: false,
     class_id: 2
   },
   {
     id:10,
-    key: 'c-major',
+    musical_key: 'c-major',
     chord: 'F-major7',
     major: true,
     class_id: 2
   },
   {
     id:11,
-    key: 'c-major',
+    musical_key: 'c-major',
     chord: 'G-dominant7',
     major: false,
     class_id: 2
   },
   {
     id:12,
-    key: 'c-major',
+    musical_key: 'c-major',
     chord: 'A-minor7',
     major: false,
     class_id: 2
